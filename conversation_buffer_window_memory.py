c5bae4d4fd271d7f8cf3a8ee085f1d7b01cfeb11
Izabel de Barros <izabelfeigel@gmail.com>
2024-05-24 11:22:35 -0300

    first commit

---
+++ conversation_buffer_window_memory.py
@@ -0,0 +1,42 @@
+import os
+import openai
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferWindowMemory
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# account for deprecation of LLM model
+import datetime
+# Get the current date
+current_date = datetime.datetime.now().date()
+
+# Define the date after which the model should be set to "gpt-3.5-turbo"
+target_date = datetime.date(2024, 6, 12)
+
+# Set the model variable based on the current date
+if current_date > target_date:
+    llm_model = "gpt-3.5-turbo"
+else:
+    llm_model = "gpt-3.5-turbo-0301"
+
+memory = ConversationBufferWindowMemory(k=1)  
+
+llm = ChatOpenAI(temperature=0.0, model=llm_model)
+memory = ConversationBufferWindowMemory(k=1)
+conversation = ConversationChain(
+    llm=llm, 
+    memory = memory,
+    verbose=False
+)
+
+conversation.predict(input="Hello, my name is Izabel!")
+conversation.predict(input="Whats the weather today in Florianópolis?")
+conversation.predict(input="Whats my name?")
+
+print(memory.buffer)
